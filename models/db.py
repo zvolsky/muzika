c7c4b58866bbf0c09aa60cf5c6c88a4aedcd331b
zvolsky <zvolsky@seznam.cz>
2015-03-20 09:26:47 +0100

    zapnuto https

--- models/db.py
+++ models/db.py
@@ -12,7 +12,11 @@
 
 ## if SSL/HTTPS is properly configured and you want all HTTP requests to
 ## be redirected to HTTPS, uncomment the line below:
-# request.requires_https()
+if request.is_local:
+    from gluon.custom_import import track_changes
+    track_changes(True)    # auto-reload modules
+else:
+    request.requires_https()
 
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
